ec8e804fa2caf53e180152c8a03393cd06681e64
JuniorSua <juniorsuarez1014@gmail.com>
2022-09-15 08:36:14 -0400

    First commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,12 @@
+// Raffle 
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiable random)
+// Winner to be selected every X minutes -> completly automated 
+// Chainlink Oracle -> Randomness, Automated Excecution (Chainlink Keepers)
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Raffle {}
+
